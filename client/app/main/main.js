10b14d5d0cc26e8169f1317605124df11cd76d6e
Bryan Leboff <leboff@gmail.com>
2015-06-19 09:53:58 -0400

    initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('profileKingApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });

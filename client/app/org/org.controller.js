1bbd4160218d9c66616f5796dff1315b005a8812
Bryan Leboff <leboff@gmail.com>
2015-07-21 13:45:07 -0400

    removed unneeded junk

--- client/app/org/org.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('profileKingApp')
-  .controller('OrgCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
--- client/app/profile/profile.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('profileKingApp')
-  .controller('ProfileCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
